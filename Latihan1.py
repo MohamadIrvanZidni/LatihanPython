bc88cef55ee2f875512b8c9f5baf1907b025b2ae
MohamadIrvanZidni <115876072+MohamadIrvanZidni@users.noreply.github.com>
2022-10-31 14:12:54 +0700

    Add files via upload

---
+++ Latihan1.py
@@ -0,0 +1,6 @@
+#menampilkan tulisan "Latihan 1" di layar 
+print("Latihan 1")
+#menampilkan tulisan "Hello World" di layar 
+print("Hello World")
+#menampilkan tulisan "Saya sedang belajar python" di layar 
+print("Saya sedang belajar python")
---
+++ Latihan2.py
@@ -0,0 +1,10 @@
+print("Latihan 2")
+
+a = input("Masukkan variabel a : ")
+b = input("Masukkan variabel b : ")
+
+print("Variabel a : ", a)
+print("Variabel b : ", b)
+total = int(a) + int(b)
+
+print("Hasil penjumlahan ", a, " + ", b, " adalah ", total)
